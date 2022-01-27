e57e681a2e4b9a5b031e5adf8886e2ebeb0c4341
sftsk <luki@booncon.com>
2022-01-27 23:05:05 +0200

    basic content there..

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Time to celebrate!</h1>
+<p>
+	As you might know by now we've got married in December 2021 and would now like to celebrate with
+	our friends!<br />
+	The party will take place on 10.09.2022 in Marlengo, Italy.<br />
+	We are looking to provide some place to sleep for people who aren't too picky and obviously we can
+	help you finding a hotel etc. if you prefer that.
+</p>
+<iframe
+	title="rsvp form"
+	src="https://docs.google.com/forms/d/e/1FAIpQLSdStoMsW2qctuRamt2IBOjRfctUvGUMLPrGkMhLs2EXZiiVFA/viewform?embedded=true"
+	width="100%"
+	height="1785"
+	frameborder="0"
+	marginheight="0"
+	marginwidth="0">Loading…</iframe
+>
