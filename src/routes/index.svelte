87cca7d0748728e7ba3926f536f95bbcff3c638a
sftsk <luki@booncon.com>
2022-01-28 18:30:58 +0200

    basic version done

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,17 +1,88 @@
-<h1>Time to celebrate!</h1>
-<p>
-	As you might know by now we've got married in December 2021 and would now like to celebrate with
-	our friends!<br />
-	The party will take place on 10.09.2022 in Marlengo, Italy.<br />
-	We are looking to provide some place to sleep for people who aren't too picky and obviously we can
-	help you finding a hotel etc. if you prefer that.
-</p>
-<iframe
-	title="rsvp form"
-	src="https://docs.google.com/forms/d/e/1FAIpQLSdStoMsW2qctuRamt2IBOjRfctUvGUMLPrGkMhLs2EXZiiVFA/viewform?embedded=true"
-	width="100%"
-	height="1785"
-	frameborder="0"
-	marginheight="0"
-	marginwidth="0">Loading…</iframe
->
+<div class="hero">
+	<div class="hero-text">
+		<h1 style="margin-bottom: 4px">Time to celebrate!</h1>
+		<h2 style="margin-top: 0px">10.09.2022 – Marlengo, Italy</h2>
+	</div>
+</div>
+<div class="container">
+	<p>
+		As you might know by now we've got married in December 2021 and it's about time to celebrate
+		with our friends.
+		<br />
+		The party will take place on 10.09.2022 in Marlengo, Italy and if the weather goods are with us take
+		place outdoors.<br /><br />
+		We are looking to provide some place to sleep for people who aren't too picky and obviously we can
+		help you find a hotel if you prefer that. Please let us know in the RSVP form.<br />
+		The easiest way to get there from Finland is to fly to Munich or Milan and take a rental car, train
+		or bus. When it's clearer who will come we can try to plan something together as much as possible.<br
+		/><br />
+		There will be food, music and hopefully loads of fun. Bring your dancing shoes and dress however
+		you want! No need for presents, if you really insist we are happy with a little something towards
+		our honeymoon: IBAN0123...
+		<br /><br />
+		Please click on the following button to fill out our RSVP from. It will make our time a lot easier.
+	</p>
+	<div style="text-align: center;">
+		<a href="https://forms.gle/nH4cDQyP48qGVZXw5"><button type="button">RSVP</button></a>
+	</div>
+	<span style="font-size: 1.6rem">We hope to see many of you!<br />😘 Karo + Luki</span>
+</div>
+
+<style>
+	.hero {
+		background: url(l+l.jpg) no-repeat;
+		background-size: cover;
+		background-position: 40% 55%;
+		height: 72vh;
+		position: relative;
+	}
+	.hero-text {
+		color: white;
+		position: absolute;
+		bottom: 40px;
+		left: 40px;
+		font-size: 1.3rem;
+	}
+
+	.container {
+		margin: 36px auto;
+		padding: 0 40px;
+		font-size: 1.4rem;
+		line-height: 1.5;
+		max-width: 70vw;
+	}
+
+	button {
+		border: none;
+		border-radius: none;
+		background: rgb(221, 160, 221);
+		font-size: 1.5rem;
+		padding: 10px 20px;
+		color: rgb(242, 233, 231);
+		cursor: pointer;
+		transition: opacity 0.2s;
+	}
+	button:hover {
+		opacity: 0.8;
+		transition: opacity 0.4s;
+	}
+	@media only screen and (max-width: 1024px) {
+		.container {
+			width: 100vw;
+		}
+	}
+
+	@media only screen and (max-width: 600px) {
+		.hero {
+			height: 42vh;
+		}
+		.hero-text {
+			left: 12px;
+			bottom: 12px;
+			font-size: 1rem;
+		}
+		.container {
+			padding: 0 12px;
+		}
+	}
+</style>
