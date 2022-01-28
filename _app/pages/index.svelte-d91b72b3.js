afef7a4fbf6a358676928aa65d250bb9b5efb5c4
Lukas Jakob Hafner <luki@booncon.com>
2022-01-28 20:10:43 +0200

    Updates

---
+++ _app/pages/index.svelte-d91b72b3.js
@@ -0,0 +1,25 @@
+import{S as pe,i as ye,s as me,e as o,t as n,k as I,c as a,a as i,h as s,d as l,m as B,f as x,b as _,g as re,J as e,K as le}from"../chunks/vendor-f0095a1c.js";function be(fe){let c,d,y,T,S,m,M,P,h,t,D,W,A,F,N,H,K,q,L,j,z,C,k,G,J,R,O,Q,U,X,Z,Y,b,w,p,$,ee,f,te,oe,ae;return{c(){c=o("div"),d=o("div"),y=o("h1"),T=n("Time to celebrate!"),S=I(),m=o("h2"),M=n("10.09.2022 \u2013 Marlengo, Italy"),P=I(),h=o("div"),t=o("p"),D=n(`As you might know by now we've got married in December 2021 and it's about time to celebrate
+		with our friends.
+		`),W=o("br"),A=n(`
+		The party will take place on 10.09.2022 in Marlengo, Italy and if the weather goods are with us takes
+		place outdoors.`),F=o("br"),N=o("br"),H=n(`
+		We are looking to provide some place to sleep for people who aren't too picky and obviously we can
+		help you find a hotel if you prefer that. Please let us know in the RSVP form.`),K=o("br"),q=n(`
+		The easiest way to get there from Finland is to fly to Munich or Milan and take a rental car, train
+		or bus. When it's clearer who will come we can try to plan something together as much as possible.`),L=o("br"),j=o("br"),z=n(`
+		There will be food, music and hopefully loads of fun. Bring your dancing shoes and dress however
+		you want!`),C=o("br"),k=o("b"),G=n("No need for presents!"),J=n(` If you really insist we are happy with a little
+		something towards our honeymoon: `),R=o("i"),O=n("FI96 5720 9920 1544 80"),Q=I(),U=o("br"),X=o("br"),Z=n(`
+		Please click on the following button to fill out our RSVP from. It will make our time a lot easier.`),Y=I(),b=o("div"),w=o("a"),p=o("button"),$=n("RSVP"),ee=I(),f=o("span"),te=n("We hope to see many of you!"),oe=o("br"),ae=n("\u{1F618} Karo + Luki"),this.h()},l(u){c=a(u,"DIV",{class:!0});var v=i(c);d=a(v,"DIV",{class:!0});var E=i(d);y=a(E,"H1",{style:!0});var ne=i(y);T=s(ne,"Time to celebrate!"),ne.forEach(l),S=B(E),m=a(E,"H2",{style:!0});var se=i(m);M=s(se,"10.09.2022 \u2013 Marlengo, Italy"),se.forEach(l),E.forEach(l),v.forEach(l),P=B(u),h=a(u,"DIV",{class:!0});var g=i(h);t=a(g,"P",{});var r=i(t);D=s(r,`As you might know by now we've got married in December 2021 and it's about time to celebrate
+		with our friends.
+		`),W=a(r,"BR",{}),A=s(r,`
+		The party will take place on 10.09.2022 in Marlengo, Italy and if the weather goods are with us takes
+		place outdoors.`),F=a(r,"BR",{}),N=a(r,"BR",{}),H=s(r,`
+		We are looking to provide some place to sleep for people who aren't too picky and obviously we can
+		help you find a hotel if you prefer that. Please let us know in the RSVP form.`),K=a(r,"BR",{}),q=s(r,`
+		The easiest way to get there from Finland is to fly to Munich or Milan and take a rental car, train
+		or bus. When it's clearer who will come we can try to plan something together as much as possible.`),L=a(r,"BR",{}),j=a(r,"BR",{}),z=s(r,`
+		There will be food, music and hopefully loads of fun. Bring your dancing shoes and dress however
+		you want!`),C=a(r,"BR",{}),k=a(r,"B",{});var ie=i(k);G=s(ie,"No need for presents!"),ie.forEach(l),J=s(r,` If you really insist we are happy with a little
+		something towards our honeymoon: `),R=a(r,"I",{});var he=i(R);O=s(he,"FI96 5720 9920 1544 80"),he.forEach(l),Q=B(r),U=a(r,"BR",{}),X=a(r,"BR",{}),Z=s(r,`
+		Please click on the following button to fill out our RSVP from. It will make our time a lot easier.`),r.forEach(l),Y=B(g),b=a(g,"DIV",{style:!0});var ue=i(b);w=a(ue,"A",{href:!0});var ce=i(w);p=a(ce,"BUTTON",{type:!0,class:!0});var de=i(p);$=s(de,"RSVP"),de.forEach(l),ce.forEach(l),ue.forEach(l),ee=B(g),f=a(g,"SPAN",{style:!0});var V=i(f);te=s(V,"We hope to see many of you!"),oe=a(V,"BR",{}),ae=s(V,"\u{1F618} Karo + Luki"),V.forEach(l),g.forEach(l),this.h()},h(){x(y,"margin-bottom","4px"),x(m,"margin-top","0px"),_(d,"class","hero-text svelte-h2sntf"),_(c,"class","hero svelte-h2sntf"),_(p,"type","button"),_(p,"class","svelte-h2sntf"),_(w,"href","https://forms.gle/nH4cDQyP48qGVZXw5"),x(b,"text-align","center"),x(f,"font-size","1.6rem"),_(h,"class","container svelte-h2sntf")},m(u,v){re(u,c,v),e(c,d),e(d,y),e(y,T),e(d,S),e(d,m),e(m,M),re(u,P,v),re(u,h,v),e(h,t),e(t,D),e(t,W),e(t,A),e(t,F),e(t,N),e(t,H),e(t,K),e(t,q),e(t,L),e(t,j),e(t,z),e(t,C),e(t,k),e(k,G),e(t,J),e(t,R),e(R,O),e(t,Q),e(t,U),e(t,X),e(t,Z),e(h,Y),e(h,b),e(b,w),e(w,p),e(p,$),e(h,ee),e(h,f),e(f,te),e(f,oe),e(f,ae)},p:le,i:le,o:le,d(u){u&&l(c),u&&l(P),u&&l(h)}}}class ve extends pe{constructor(c){super();ye(this,c,null,be,me,{})}}export{ve as default};
