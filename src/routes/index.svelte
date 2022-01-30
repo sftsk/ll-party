6af404c1e92e2bcc8262bc83aa7f3010706b5b89
sftsk <luki@booncon.com>
2022-01-30 21:15:04 +0200

    text updates

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -9,19 +9,18 @@
 		As you might know by now we've got married in December 2021 and it's about time to celebrate
 		with our friends.
 		<br />
-		The party will take place on 10.09.2022 in Marlengo, Italy and if the weather goods are with us takes
-		place outdoors.<br /><br />
+		The party will take place on 10.09.2022 in Marlengo, Italy (outdoors if the weather goods are with
+		us).<br /><br />
 		We are looking to provide some place to sleep for people who aren't too picky and obviously we can
-		help you find a hotel if you prefer that. Please let us know in the RSVP form.<br />
+		help you find a hotel if that's what you prefer. Please let us know in the RSVP form.<br />
 		The easiest way to get there from Finland is to fly to Munich or Milan and take a rental car, train
-		or bus. When it's clearer who will come we'll try to organise something together as much as possible.<br
-		/><br />
+		or bus. When it's clearer who will come we'll try to organise transportation together as much as
+		possible.<br /><br />
 		There will be food, music and hopefully loads of fun. Bring your dancing shoes and dress however
-		you want.<br /><b>No need for presents!</b> If you really insist we are happy with a little
-		something towards our honeymoon: <i>FI96 5720 9920 1544 80</i>
+		you want.<br /><b>No need for presents, your presence alone is all we want!</b> If you really
+		insist we are happy with a little something towards our honeymoon: <i>FI96 5720 9920 1544 80</i>
 		<br /><br />
-		Please click on the following button to fill out our RSVP from. It will make our time a lot easier
-		🙏
+		Please click on the following button to fill out our RSVP form before 31.07.2022 🙏
 	</p>
 	<div style="text-align: center;">
 		<a href="https://forms.gle/nH4cDQyP48qGVZXw5"><button type="button">RSVP</button></a>
